bfe930cf76f920e9deddcc54a70dd308fbe8d70b
ManuelHMR <manuelhmrjf@gmail.com>
2022-07-23 18:18:17 -0300

    feat: signup jest test

---
+++ tests/auth.test.ts
@@ -0,0 +1,37 @@
+import app from "./../src/app";
+import supertest from "supertest";
+import { prisma } from "./../src/config/database";
+  
+afterAll(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+    await prisma.$disconnect();
+});
+
+const USER = {
+    email: "teste@teste.com",
+    password: "1234567890"
+}
+
+describe("POST /signup", ()=> {
+    it("create user", async ()=> {
+        const response = await supertest(app).post("/signup").send(USER);
+        expect(response.statusCode).toBe(201);
+    });
+    it("create a user with an email already in use", async ()=> {
+        const response = await supertest(app).post("/signup").send(USER);
+        expect(response.statusCode).toBe(409);
+    });
+});
+
+// describe("POST /signin", ()=> {
+//     it("login with valid credentials", async ()=> {
+//         const response = await supertest(app).post("/signin").send(USER)
+//         const token = response.body.token;
+//         expect(token).not.toBeNull();
+//     });
+//     it("login without valid credentials", async ()=> {
+//         const response = await supertest(app).post("/signin").send({...USER, password: "0000000000"})
+//         expect(response.statusCode).toBe(400);
+//     });
+// });
+
